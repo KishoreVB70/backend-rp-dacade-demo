f340e49d78b534d233865408d3208968d66e3787
Kishore <kishore07kzr@gmail.com>
2024-10-22 07:59:17 +0530

    feat: added validate_allinputs method to take all the required inputs from the front end to modularize the code

--- src/rpdemo_backend/src/lib.rs
+++ src/rpdemo_backend/src/lib.rs
@@ -15,6 +15,14 @@ thread_local! {
     static SETTINGS: RefCell<Option<Settings>> = const { RefCell::new(None) };
 }
 
+#[derive(Clone, Debug, CandidType, Deserialize, Eq, PartialEq)]
+pub struct ValidateVpRequest {
+    pub vp_jwt: String,
+    pub effective_vc_subject: Principal,
+    pub credential_spec: CredentialSpec,
+    pub issuer_origin: String,
+}
+
 // Main function
 #[query]
 #[candid_method]
@@ -35,7 +43,6 @@ fn validate_vc_token(vp_jwt: String) -> String {
             // Front settings
             ii_canister_id: settings.ii_canister_id,
             ii_origin: "https://identity.ic0.app/".to_string(),
-
             // From settings
             issuer_canister_id: settings.issuer_canister_id,
             issuer_origin: "https://dacade.org/".to_string(),
@@ -72,6 +79,55 @@ fn validate_vc_token(vp_jwt: String) -> String {
     })
 }
 
+
+#[query]
+#[candid_method]
+fn validate_vc_token_allinputs(req: ValidateVpRequest) -> String {
+    SETTINGS.with_borrow(|settings_opt| {
+        let settings = settings_opt
+            .as_ref()
+            .expect("Settings should be initialized");
+
+        // Current system time in nanoseconds, used for validation timestamp
+        let current_time_ns: u128 = ic_cdk::api::time() as u128;
+
+        // Define the two signers involved in the Verifiable Credential flow
+        let vc_flow_signers = VcFlowSigners {
+            // Front settings
+            ii_canister_id: settings.ii_canister_id,
+            ii_origin: "https://identity.ic0.app/".to_string(),
+            // From settings
+            issuer_canister_id: settings.issuer_canister_id,
+            issuer_origin: req.issuer_origin,
+        };
+
+        // Define the expected credential specification for the VP. This spec should match the
+        // credential type and argument values in the VP.
+        let vc_spec = req.credential_spec;
+        // Unique identifier of the caller principal
+        let effective_vc_subject = req.effective_vc_subject;
+        let vp_jwt = req.vp_jwt;
+
+        // Validate the VP JWT against the specified signers and credential specifications
+        match validate_ii_presentation_and_claims(
+            // Input string
+            &vp_jwt,
+            // User principal
+            effective_vc_subject,
+            // We're creating, partially from settings
+            &vc_flow_signers,
+            // Creating
+            &vc_spec,
+            // Only thing obtained from the settings
+            &settings.ic_root_key_raw,
+            // Obtaining
+            current_time_ns,
+        ) {
+            Ok(_) => "✅ Success, the credential is valid.".to_string(),
+            Err(e) => format!("🛑 Error: {:?}", e),
+        }
+    })
+}
 // init and upgrade logic
 #[candid_method(init)]
 #[init]
